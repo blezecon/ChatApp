4a0eea902b01ff5b3d8adbe484de7aa030d71dd9
blezecon <dfyyasdd@gmail.com>
2025-06-11 20:58:42 +0530

    frontend 2nd phase

---
+++ frontend/src/store/useAuthStore.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+import {create} from 'zustand';
+import { axiosInstance } from '../lib/axios';
+
+
+export const useAuthStore = create((set) => ({
+    authUser: null,
+    isSigninUp: false,
+    isLoginIn: false,
+    isUpdatingProfile: false,
+
+
+    isCheckingAuth: true,
+
+    checkAuth: async() => {
+        try {
+            const res = await axiosInstance.get('/auth/check');
+
+            set({authUser: res.data});
+        } catch (error) {
+            console.log('Error in checking auth:', error);
+            set({authUser: res.null});
+        }finally{
+            set({isCheckingAuth: false});
+        }
+    }
+}));
