79076040aef266d79a7dcf138fd1d21d82ffb1fc
blezecon <dfyyasdd@gmail.com>
2025-06-11 09:01:23 +0530

    message route add

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,8 @@
 import express from "express"
 import dotenv from "dotenv"
 import authRoutes from "./routes/auth.route.js"
+import messageRoutes from "./routes/message.route.js"
+
 import { connect } from "mongoose";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
@@ -15,7 +17,8 @@ const PORT =process.env.PORT;
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/api/auth", authRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 app.listen(PORT, () =>  {
     console.log("Server is running on port:"+ PORT);
