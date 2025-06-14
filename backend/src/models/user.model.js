81276f9e86bfdbe3c83dd37f0633e9be0b6b5a3e
blezecon <dfyyasdd@gmail.com>
2025-06-14 22:00:54 +0530

    backend full done

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,30 +1,29 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
-    {
-        email:{
-            type: String,
-            required: true,
-            unique: true,
-            lowercase: true,
-        },
-        password: {
-            type: String,
-            required: true,
-        },
-        fullName: {
-            type: String,
-            required: true,
-        },
-        profilePic:{
-            type: String,
-            default: "",
-        }
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    {
-        timestamps: true,
-    }
+    fullName: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+    profilePic: {
+      type: String,
+      default: "",
+    },
+  },
+  { timestamps: true }
 );
 
 const User = mongoose.model("User", userSchema);
+
 export default User;
--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,11 +1,11 @@
-import express from 'express';
-import { protectRoute } from '../middleware/auth.middleware.js';
-import { getUsersForSidebar, getMessages, sendMessage } from '../controllers/message.controller.js';
+import express from "express";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import { getMessages, getUsersForSidebar, sendMessage } from "../controllers/message.controller.js";
 
 const router = express.Router();
 
 router.get("/users", protectRoute, getUsersForSidebar);
-router.get("/users/:userId", protectRoute, getMessages);
+router.get("/:id", protectRoute, getMessages);
 
 router.post("/send/:id", protectRoute, sendMessage);
 
